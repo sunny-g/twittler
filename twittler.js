a76bfbe1e7a3f30704a58717fd9c4ece7ca585ec
sunny-g <sunny.gonna@gmail.com>
2014-09-30 14:49:42 -0500

    finished twittler (technically)
    it looks god-awful so it needs to be reformatted:
    - dates need to be reformatted and on not concatenated w/ the tweet
    - the tweet itself needs to look like a tweet:
    -- smaller well
    -- bolded, italicized, greyed text
    -- i think that's it for now

--- twittler.js
+++ twittler.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
   // if you add user, create new pill
   // make first pill active
   var $navbar = $('<ul class="nav nav-pills"></ul>');
-  var $pill = $('<li class="users-tweets"></li>');
+  var $pill = $('<li class="active users-tweets"></li>');
   var $link = $('<a href="#">All Tweets</a>');
   $navbar.append($pill.append($link));
   users.forEach(function(user) {
@@ -23,29 +23,57 @@ $(document).ready(function(){
     $pill.append($link);
     $navbar.append($pill)
   });
-  $($navbar.find('li')[0]).addClass('active');
-  $body.append($navbar);
+  // $($navbar.find('li')[0]).addClass('active');
+  $('.page-header').after($navbar);
 
   $('.nav').on('click', '.users-tweets', function(e) {
     e.preventDefault();
+
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
+
+    var tabName = $(this).find('a').text();
+    displayTweets(tabName);
   });
 
 
 
+  function displayTweets(name) {
+    // console.log('displaying tweets...');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="well span6"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
+    name = name || $('li.users-tweets.active').find('a').text();
+
+    var $tweetBox = $('#tweetBox');
+    $tweetBox.html('');
+
+    var index;
+    var tweetList;
+    if (name === 'All Tweets') {
+      tweetList = streams.home;
+      index = tweetList.length - 1;
+    } else {
+      name = name.slice(1, name.length);
+      // console.log(name)
+      tweetList = streams.users[name];
+      index = tweetList.length - 1;
+    }
+
+    while (index >= 0) {
+      var tweet = tweetList[index];
+      var $tweet = $('<div class="well span6"></div>');
+      var text = '@' + tweet.user + ': ' + tweet.message + tweet.created_at;
+      // $tweet.text()
+      $tweet.appendTo($tweetBox);
+      index -= 1;
+
+    }
   }
 
 
 
+/*
+autorefresh = displayTweets on a timer
+ */
 
 
 
